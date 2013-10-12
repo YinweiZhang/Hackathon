3bc5e9aeb5693432b1eb81cb10e277c5a0fe0feb
YinweiZhang <qzhao66@neo.tamu.edu>
2013-10-12 01:53:47 -0500

    Setting rails up.

---
+++ Relp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Relp::Application.config.secret_token = 'cbd0ea74281437927bd297d2a425f8cfac2c3f7a49f927f7cf446585906676a3a053890188794eaf9fcb69fb10575653421e05d023eab280ac65a9b60d75b1c7'
